f8352cd86e7d95de12e09440548ad3549901a050
ejrach <eric.rach@gmail.com>
2019-07-29 12:06:51 -0500

    Updated for completion of Django tutorial

--- PyShop/products/views.py
+++ PyShop/products/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 
 # The '/products' page maps 'index'
@@ -12,7 +13,8 @@ from django.shortcuts import render
 
 # - Index will be the main page of the 'products' page.
 def index(request):
-    return HttpResponse('Hello World')
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products':products})
 
 
 # - new will be the page browsed to at '/products/new' page.
