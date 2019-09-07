a11582385b2a4fc008e408c2a2b7c921e208fc64
ejrach <eric.rach@gmail.com>
2019-09-07 13:01:48 -0500

    Added a property to Customer class and add-migrations in small increment

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -7,9 +7,15 @@ namespace Vidly.Models
 {
     public class Customer
     {
+        //Tip: after adding a property it's a good idea to create migrations in small increments:
+        //After adding a property like "IsSubscribedToNewsletter, go to PM
+        //  PM > add-migration AddIsSubscribedToCustomer
+        //  PM > Update-Database
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
 
     }
 }
