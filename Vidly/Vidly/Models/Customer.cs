cfca9b37968c15baf285d90f73442185deb4a12e
ejrach <eric.rach@gmail.com>
2019-09-07 13:32:24 -0500

    added migrations

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.Web;
 
 namespace Vidly.Models
@@ -13,6 +14,8 @@ namespace Vidly.Models
         //  PM > Update-Database
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
