a791e6440846fa894facdb5a077fb0f1f2ac9535
ejrach <eric.rach@gmail.com>
2019-09-09 12:04:01 -0500

    Added query of the database through CustomersController

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -9,30 +10,44 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        //In order to query the database, we need to create a database context. 
+        private ApplicationDbContext _context;
+
+        //Create a constructor for the CustomersController that creates the DbContext for this
+        //controller.
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //The DbContext is a disposable object, se we need to properly dispose of the object.
+        //So the proper way to do it is to override the dispose method of the base controller class.
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            //the customers property is a dbset we defined in our DbContext.
+            //A query to the database will immediately occur with the "ToList()" method.
+            //We are using "Include" method here to load the Customers along with their membership types
+            //together. This is called "Eager Loading". "c" is customer, and it goes to c.MembershipType.
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            //A query to the database will immediately occur with the "SingleOrDefault()" method.
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
         }
     }
 }
