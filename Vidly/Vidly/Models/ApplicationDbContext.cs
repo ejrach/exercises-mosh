f33ee5f21c0974408fba9e4e75e9245c0041da1c
ejrach <eric.rach@gmail.com>
2019-09-21 11:07:36 -0500

    Split out ApplicationDbContext and ApplicationUser into their own classes

---
+++ Vidly/Vidly/Models/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Vidly.Models
+{
+    //The IdentityDbContext is the gateway to our database. It is part of Asp.net identity framework.
+    //So when we execute enable-migration, Entity Framework (EF) looked at our IdentityDbContext and it discovered
+    //Db sets in IdentityDbContext like user, role, etc.
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        //Add DbSets
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
--- Vidly/Vidly/Models/ApplicationUser.cs
+++ Vidly/Vidly/Models/ApplicationUser.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
 using System.Security.Claims;
-using System.Web;
 using System.Threading.Tasks;
-using System.Data.Entity;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.ComponentModel.DataAnnotations;
@@ -28,28 +24,6 @@ namespace Vidly.Models
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
-        }
-    }
-
-    //The IdentityDbContext is the gateway to our database. It is part of Asp.net identity framework.
-    //So when we execute enable-migration, Entity Framework (EF) looked at our IdentityDbContext and it discovered
-    //Db sets in IdentityDbContext like user, role, etc.
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        //Add DbSets
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Movie> Movies { get; set; }
-        public DbSet<MembershipType> MembershipTypes { get; set; }
-        public DbSet<Genre> Genres { get; set; }
-
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
         }
     }
 }
