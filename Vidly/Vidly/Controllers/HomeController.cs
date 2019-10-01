f120c7de462feb99ee760f5f4f21e273410dbba0
ejrach <eric.rach@gmail.com>
2019-10-01 09:19:26 -0500

    Added Elmah package

--- Vidly/Vidly/Controllers/HomeController.cs
+++ Vidly/Vidly/Controllers/HomeController.cs
@@ -22,8 +22,6 @@ namespace Vidly.Controllers
 
         public ActionResult About()
         {
-            throw new Exception();
-
             ViewBag.Message = "Your application description page.";
 
             return View();
